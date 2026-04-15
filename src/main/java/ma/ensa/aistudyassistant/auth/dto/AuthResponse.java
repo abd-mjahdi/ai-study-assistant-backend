494e0a73d89c4600e7699a5857d0cdb93191fe71
abd-mjahdi <abdelouahabmjahdi@gmail.com>
2026-04-15 15:06:26 +0100

    authentication and jwt implementation

---
+++ src/main/java/ma/ensa/aistudyassistant/auth/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+package ma.ensa.aistudyassistant.auth.dto;
+
+import java.util.UUID;
+
+public record AuthResponse(
+        String token,
+        UUID userId,
+        String username,
+        String email
+) {
+}
